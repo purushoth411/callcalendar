0599f679b98414c288eeec636750e19b1c309f9c
sarorosy <sn11std@gmail.com>
2025-08-21 12:54:45 +0530

    team table design

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -342,7 +342,7 @@ export default function Teams() {
               <DataTable
                 data={teams}
                 columns={columns}
-                className="display table table-auto w-full text-[12px] border border-gray-300 n-table-set dataTable"
+                className="table-auto w-full text-[12px] border border-gray-300 divide-y divide-gray-300 dataTable the-table-set"
                 options={tableOptions}
               />
             )}
